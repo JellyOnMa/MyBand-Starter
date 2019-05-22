64c816a7331dcc0be103a20fc019e9b1bc7e7512
Jelle Stekelenburg <43231246+JellyOnMa@users.noreply.github.com>
2019-05-22 09:53:49 +0200

    Add files via upload

--- public/index.php
+++ public/index.php
@@ -1,31 +1,119 @@
-<?php
-/**
- * Dit is de Front controller
- * ALLE requests die naar de webserver worden gedaan worden via de .htaccess naar dit script gestuurd.
- * Zo voorkom je dat je heel veel PHP bestanden hebt met dubbele code en logica.
- *
- * Alle urls hebben de volgende structuur: http://localhost/<controller>/<actie>/<optionele parameters>
- *
- * Bijv: http://localhost/page/show/about-us
- * (roept de show functie aan in de page controller met de parameter 'about-us')
- *
- * controller: page
- * actie: show
- * parameter: about-us
- *
- * De Front Controller (dit bestand) doorloopt elke keer de volgende procedure:
- *
- * 1. Inspecteert de URL die wordt opgevraagd
- * 2. Zoekt op in een "routing" tabel of hij deze URL of dit URL patroon kent
- * 3. Zo niet, dan is het een 404 oagina, want de website kent deze url niet
- * 4. Als de url wel wordt herkend en er een "route" is naar de juiste code dan:
- *  A. Wordt de juiste controller actie aangeroepen
- *  B. De controller krijgt alle gegevens door (de url, $_GET, $_POST, $_FILES etc)
- *  C. De controller haalt eventueel gegevens op via de Model laag (database queries e.d.)
- *  D. De controller geeft de gegevens aan de juiste view
- *  E. De view toont de gegevens op de juiste manier (met behulp van een foreach, if, switch e.d.)
- *  F. De complete view (met data) wordt door de controller teruggestuurd naar de gebruiker (dit is de Response)
- *
- */
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+
+    <?php
+    require __DIR__ . '/private/functions.php';
+    $database = dbConnect();
+    ?>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSS files -->
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/nav.css">
+    <link rel="stylesheet" href="css/box.css">
+    <link rel="stylesheet" href="css/ajax.css">
+
+    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
+    <script src="ajax.js"></script>
+    <script>
+    function nav_trigger() {
+      document.getElementById("navbar").style.background = "rgba(0,0,0,.6)";
+}
+    </script>
 
 
+    <title>Trippie Redd - Fanpage</title>
+  </head>
+  <body onscroll="nav_trigger()">
+
+    <!-- Navigatie -->
+    <div id="navbar">
+      <div class="nav_left">
+        <h3><a href="#index.php">Trippie <span class="red">Redd</span></a></h3>
+        <img src="img/1400.png" alt="">
+      </div>
+      <div class="nav_right">
+        <a href="#">About</a>
+        <a href="#">News</a>
+        <a href="#">Agenda</a>
+        <a href="#">Contact</a>
+      </div>
+    </div>
+
+    <!-- header -->
+    <div class="header">
+      <h3>Trippie <span class="red">Redd</span></h3>
+      <p class="quote_header">"big fourteen"</p>
+    </div>
+
+    <br><br>
+    <div class="main">
+      <div class="box_1">
+        <?php
+        define("RIJ_PER_PAGINA",2);
+
+        //zoeken
+        $zoek_keyword = '';
+        if(!empty($_POST['search']['keyword'])) {
+          $zoek_keyword = $_POST['search']['keyword'];
+        }
+
+        $sql = 'SELECT * FROM posts WHERE id = 1';
+
+
+        // Pagination
+        $per_pagina_html = '';
+        $pagina = 1;
+        $start = 0;
+        if(!empty($_POST["page"])) {
+          $pagina = $_POST["page"];
+          $start = ($pagina-1) * RIJ_PER_PAGINA;
+        }
+        $limit = " limit " . $start . "," . RIJ_PER_PAGINA;
+
+        // PDO statement
+        $pagination_statement = $database->prepare($sql);
+        $pagination_statement->bindValue(':keyword', '%' . $zoek_keyword . '%', PDO::PARAM_STR);
+        $pagination_statement->execute();
+
+
+        $query = $sql.$limit;
+        $pdo_statement = $database->prepare($query);
+        $pdo_statement->bindValue(':keyword', '%' . $zoek_keyword . '%', PDO::PARAM_STR);
+        $pdo_statement->execute();
+        $result = $pdo_statement->fetchAll();
+        ?>
+
+            <tbody id='tabel-body'>
+              <?php
+              if(!empty($result)) {
+                foreach($result as $row) {
+                  ?>
+
+                    <h3 class="tabel_title"><?php echo $row['titel']; ?></h3>
+                    <p class="tabel_text"><?php echo $row['omschrijving']; ?></p>
+
+                  <?php
+                }
+              }
+              ?>
+
+      </div>
+
+      <div class="box_2">
+        <div class="box">
+          <div class="layer"></div>
+          <div class="layer"></div>
+          <div class="layer"></div>
+          <div class="layer"></div>
+        </div>
+      </div>
+    </div>
+
+
+
+  </body>
+</html>
